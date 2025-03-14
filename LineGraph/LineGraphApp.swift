020ee6d00a248f8305a6e51e525bb5190c27a8ff
John Yung <johnnyyungy@gmail.com>
2025-03-14 13:56:36 +0000

    Initial Commit

---
+++ LineGraph/LineGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineGraphApp.swift
+//  LineGraph
+//
+//  Created by Yung Bros on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LineGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
